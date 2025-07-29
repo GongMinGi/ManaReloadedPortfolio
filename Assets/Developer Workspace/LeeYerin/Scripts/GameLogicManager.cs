090f2b900ba0013d124a56495c3aeb2c12957897
Mellling <bluebell1183@gmail.com>
2025-07-29 18:29:00 +0900

    feat: 플레이어 사망과 게임 종료 로직 연결 및 플레이어 오브젝트 설정
    - GameLogicManager의 GameOver()에 IsGameOver를 true로 변경하는 내용 추가
    - 플레이어 사망 시 GameLogicManager의 GameOver() 호출
    - EnemyManager에 GameLogicManager의 IsGameOver가 true일 경우에는 적 스폰 종료하도록 조건 추가
    - 플레이어에 올바른 애니메이션 컨드롤러 할당
    - 플레이어에 빠진 이벤트 할당

--- Assets/Developer Workspace/LeeYerin/Scripts/GameLogicManager.cs
+++ Assets/Developer Workspace/LeeYerin/Scripts/GameLogicManager.cs
@@ -27,9 +27,10 @@ public class GameLogicManager : MonoBehaviour
     Coroutine timer;            // 게임 시간 추적용 코루틴 핸들
     #endregion
 
-    #region Game Over UI Setting
+    #region Game Over Setting
     [SerializeField] GameObject gameOverUI;     // 게임 오버 UI 오브젝트
     [SerializeField] TMP_Text totalPlayTimeText;    // 총 게임 진행 시간 텍스트
+    public bool IsGameOver { get; set; } = false;
     #endregion
 
     #region State
@@ -84,6 +85,7 @@ public class GameLogicManager : MonoBehaviour
     /// </summary>
     public void GameOver()
     {
+        IsGameOver = true;
         StopCoroutine(timer);   // 타이머 종류
         totalPlayTimeText.text = $"{time / 60:D2} : {time % 60:D2}";    // 총 플레이 시간 텍스트 설정
         gameOverUI.SetActive(true);     // 게임 오버 UI 활성화
