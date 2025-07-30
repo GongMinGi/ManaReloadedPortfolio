cf4ef4ce2629bc8c6087cbeb4465cb5cb5be9537
Mellling <bluebell1183@gmail.com>
2025-07-30 14:48:33 +0900

    feat: 게임 프로그램 처음 시작 시 조작 법 설명 UI 활성화
    - 처음 시작 시 조작 법 설명 UI 활성화 및 팝업 스택에 추가
    - 이후 게임 종료 후 다시 메인 메뉴로 돌아오면 UI 비활성화

--- Assets/Developer Workspace/LeeYerin/Scripts/PreGame/PreGameFlowManager.cs
+++ Assets/Developer Workspace/LeeYerin/Scripts/PreGame/PreGameFlowManager.cs
@@ -10,7 +10,8 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class PreGameFlowManager : MonoBehaviour
 {
-    [Header("")]
+    [Header("UI Setting")]
+    [SerializeField] PopupController controlGuideUI; // 조작 법 설명 UI
     [SerializeField] GameObject gameMenuUI;     // 게임 메뉴 UI
     [SerializeField] GameObject loadOutUI;      // 로드아웃 UI
     [SerializeField] Animator playerAnimator;   // 로드아웃의 플레이어 애니메이터
@@ -26,7 +27,12 @@ public class PreGameFlowManager : MonoBehaviour
         GameModeManager.SoundManager.PlayBGM(bgmClip.title);    // PreGameFlow의 BGM 실행
 
         if (GameModeManager.UIManager.IsFirstLaunch)    // 게임 실행 후 첫 진입일 경우
+        {
+            GameModeManager.UIManager.OpenPopup(controlGuideUI);    // 조작 법 설명 UI를 팝업 스택에 Push
             yield break;
+        }
+
+        controlGuideUI.Backdrop.SetActive(false);   //  // 조작 법 설명 UI 비활성화
 
         GameModeManager.UIManager.ClearPopupHistory();      // UIManager의 ClearPopupHistory 스택 초기화
 
