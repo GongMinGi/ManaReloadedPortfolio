a48717188a0a6b8790b1294d1e6b3f60d70abe87
Jiho Shin <tlswlgh2001@naver.com>
2025-09-28 16:58:33 +0900

    feat: BGM 및 SFX 리소스 추가, 메인씬 BGM 출력
    제작완료된 SFX 리소스 추가 및 사운드뱅크 추가&수정, 메인씬에서 BGM 출력 테스트 및 완료(공민기 폴더의 Main Menu & Loadout Scene 씬)

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BGM_DUMMY_MAINTITLE = 1990083467U;
+        static const AkUniqueID PLAY_BGM_DUMMY_STAGE01 = 1306791755U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_BUTTON_01 = 113452694U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_BUTTON_02 = 113452693U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_BUTTON_03 = 113452692U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_BUTTON_04 = 113452691U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_BUTTON_05 = 113452690U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_BUTTON_06 = 113452689U;
+        static const AkUniqueID PLAY_SFX_PLAYER_CAST_START = 4230574424U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_FIRE_END = 4189576160U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_FIRE_LOOP = 3669469019U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_FIRE_READY = 1802017886U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_FIRE_SHOT = 3384474145U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_LIGHT_READY = 3186217726U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_LIGHT_SHOT = 317117249U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_STONE_READY = 3327860805U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_STONE_SHOT = 3098527288U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_THUNDER_READY = 3694778788U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_THUNDER_READY_LOOP = 679765885U;
+        static const AkUniqueID PLAY_SFX_PLAYER_RANGE_THUNDER_SHOT = 3382960767U;
+        static const AkUniqueID PLAY_SFX_PLAYER_SPELL_HEAL = 3736043187U;
+        static const AkUniqueID PLAY_SFX_PLAYER_SPELL_METEOR = 1703588885U;
+        static const AkUniqueID PLAY_SFX_PLAYER_SPELL_SPIKE = 3010407773U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BANKMAIN = 3634050144U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BGM = 412724365U;
+        static const AkUniqueID MASTER = 4056684167U;
+        static const AkUniqueID MONSTER = 2376328173U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID STINGER = 78360149U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
